434a83bd529264ac260edec4978430b0b8e2e99c
--unset <--unset>
2022-02-14 16:01:38 -0800

    update

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ $(document).ready(function(){
       $(this).attr("src", "images/spotify.png");
     });
   });
+
+$("#button").click(function(){
+  // let bg = document.querySelector("body").style.backgroundColor;
+  // if (bg == "black") {
+  //   document.querySelector("body").style.backgroundColor = "white";
+  // }
+  // else {
+  //   document.querySelector("body").style.backgroundColor = "black";
+  // }
+}); 
