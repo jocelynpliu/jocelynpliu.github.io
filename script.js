c2f4e28c37fbd24707e38c961ff2a24c77a9a5b5
--unset <--unset>
2022-02-15 01:54:52 -0800

    update actor page

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
 
 
 document.onload = function() {
-    $("#main-img").fadeIn();
+    $(".main-img").fadeIn();
 };
 
 $(document).ready(function(){
-    $("#spotify").hover(function(){
-      $(this).attr("src", "assets/spotify.gif");
-      }, function(){
-      $(this).attr("src", "assets/spotify.png");
-    });
-});
+  $("#spotify").hover(function(){
+    $(this).attr("src", "assets/spotify.gif");
+    }, function(){
+    $(this).attr("src", "assets/spotify.png");
+  });
 
-$(document).ready(function(){
-  $("#main-img").hover(function(){
+  $(".main-img").hover(function(){
     $("#contact-me").show();
     }, function(){
     $("#contact-me").hide();
   });
+
+  $("#google").hover(function(){
+    $("#google-stills").show();
+    }, function(){
+    $("#google-stills").hide();
+  });
 });
 
 
-
 $("#button").click(function(){
-  // let bg = document.querySelector("body").style.backgroundColor;
-  // if (bg == "black") {
-  //   document.querySelector("body").style.backgroundColor = "white";
-  // }
-  // else {
-  //   document.querySelector("body").style.backgroundColor = "black";
-  // }
+  let bg = document.querySelector("body").style.backgroundColor;
+  if (bg == "black") {
+    document.querySelector("body").style.backgroundColor = "white";
+
+    $("#cs").show();
+    $("#acting").hide();
+
+    
+    $("#button").attr("src", "assets/mask.png");
+
+    document.querySelector("#footer").style.color = "black";
+  }
+  else {
+    document.querySelector("body").style.backgroundColor = "black";
+    $("#cs").hide();
+    $("#acting").show();
+
+    $("#button").attr("src", "assets/sad-mask.png");
+
+    document.querySelector("#footer").style.color = "white";
+    
+  }
 }); 
