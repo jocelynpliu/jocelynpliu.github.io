3444d81ef78f5948fb8a2cbecf27caca69d01046
jocelyn liu <jocelynliu2000@gmail.com>
2022-10-12 22:45:27 -0700

    revert pre-PHP

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-define('DB_HOST', '303.itpwebdev.com');
-define('DB_USER', 'jliu6011');
-define('DB_PASS', 'itpclass!');
-define('DB_NAME', 'jliu6011_final_db');
-
-?>
--- footer.php
+++
@@ -1,3 +0,0 @@
-<div class="container" id="footer">
-        <p>Created by Jocelyn Liu 2022</p>
-    </div>
